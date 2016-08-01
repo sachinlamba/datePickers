f2132616321e3959cffa2134ebd94ed2552e3e2f
sachin lamba <sachin.lamba@moldedbits.com>
2016-08-01 13:21:25 +0530

    2 date pickers.1 in text field and other in storyboard

--- datePicker/ViewController.swift
+++ datePicker/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstLabelForDate: UILabel!
+    @IBOutlet weak var textFieldDate: UITextField!
+    
+    @IBAction func datePickerValueChanged(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+          dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+//        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        firstLabelForDate.text = dateFormatter.stringFromDate(sender.date)
+    }
 
+    @IBAction func textFieldDatePicker(sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 40, height: 20))
+        toolBar.barStyle = UIBarStyle.Default
+        toolBar.backgroundColor = UIColor.greenColor()
+        toolBar.items = [UIBarButtonItem(title: "Done", style: .Done, target: self, action: #selector(doneButton))]
+        
+       // datePickerView.inputAccessoryView = toolBar
+        textFieldDate.inputAccessoryView = toolBar
+        datePickerView.addTarget(self, action: #selector(datePickerChanged), forControlEvents: .ValueChanged)
+    }
+    
+    func datePickerChanged(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        
+        textFieldDate.text = dateFormatter.stringFromDate(sender.date)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func doneButton() {
+        textFieldDate.resignFirstResponder()
     }
-
-
 }
 
